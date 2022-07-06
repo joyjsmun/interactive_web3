f8c7a29fa8f8a4a7c46145bc944d7c2e922880fe
joyjsmun <joy.js.mun@gmail.com>
2022-07-07 02:10:52 +0900

    ADD: page contents

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 const Container = styled.div``
 
+const Title = styled.h1``
+
 function App() {
   return (
     <Container>
@@ -21,7 +23,57 @@ function App() {
           <a href="#">Purchase</a>
         </div>
       </nav>
+      {/* scroll section */}
+      <section className="scroll-section" id="scroll-section-0">
+          <Title>LifeIs Joy</Title>
+          <div className="sticky-elem main-message">
+            <p>I Am Joy Mun</p>
+          </div>
+          <div className="sticky-elem main-message">
+            <p>Born in Asia,<br/>raised by the world.</p>
+          </div>
+          <div className="sticky-elem main-message">
+            <p>Currently living and working in Korea and <br/>sometime in San Francisco.</p>
+          </div>
+          <div className="sticky-elem main-message">
+            <p>You're <br/>more than welcome</p>
+          </div>
+          <p></p>
+      </section>
 
+      <section className="scroll-section" id="scroll-section-1">
+        <p>
+          <strong>About Me</strong><br/>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+      </section>
+
+      <section className="scroll-section" id="scroll-section-2">
+      <div className="sticky-elem main-message">
+        <p>
+          <small>Work 1</small><br/>
+        </p>
+      </div>
+      <div className="sticky-elem desc-message">
+        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',</p>
+        <div className="pin"></div>
+      </div>
+      <div className="sticky-elem desc-message">
+        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',</p>
+        <div className="pin"></div>
+      </div>
+      </section>
+
+      <section className="scroll-section" id="scroll-section-3">
+        <p className="mid-message">
+          <strong>Contact</strong>
+          There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable
+        </p>
+        <p className="canvas-caption">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
+      </section>
+      <footer className="footer">
+        2022,LifeIsJoy
+      </footer>
     </Container>
 
   );
