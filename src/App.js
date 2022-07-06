21df14454d12f9c6e9763cb01d12efcd3ba05671
joyjsmun <joy.js.mun@gmail.com>
2022-07-07 01:10:17 +0900

    ADD: first strucuture

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import styled from "styled-components";
+
+const Container = styled.div``
 
 function App() {
   return (
-    <div>
+    <Container>
+      <nav className="global-nav">
+        <div className="global-nav-links">
+          <a className="global-nav-item" href="#">Room</a>
+          <a className="global-nav-item" href="#">Store</a>
+          <a className="global-nav-item" href="#">Idea</a>
+          <a className="global-nav-item" href="#">Contact</a>
+        </div>
+      </nav>
+      <nav className="local-nav">
+        <div className="local-nav-links">
+          <a className="product-name" href="#">LifeIs Joy</a>
+          <a href="#">About</a>
+          <a href="#">Work</a>
+          <a href="#">Purchase</a>
+        </div>
+      </nav>
 
-    </div>
+    </Container>
+
   );
 }
 
